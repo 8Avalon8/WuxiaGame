962ccf26f8ff059face7ef5121c261cc11f6d780
8Avalon8 <8avalon8@gmail.com>
2018-10-07 22:46:07 +0800

    Dialog

---
+++ Assets/Dialog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+
+    public Image m_RoleHeadImg;
+    public Text m_ContentText;
+
+    private string m_Content;
+    // Use this for initialization
+    void Start()
+    {
+        m_ContentText.text = "";
+        m_Content = "sdfsdfsldkfjslkdfjlxsckvjlskdfjlsdcfkj";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        int curTextLength = m_ContentText.text.Length;
+        if (m_ContentText.text.Length < m_Content.Length)
+        {
+            m_ContentText.text += m_Content[curTextLength];
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
